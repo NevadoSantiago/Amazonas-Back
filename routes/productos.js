f37a8f19f1aa5453bb0beffe0abb6b43118ddc66
Ricardo Capelli <ricki.capelli@gmail.com>
2020-11-20 10:03:57 -0300

    dentro de la ruta /productos, se incluyeron los siguientes cambios: -la existencia para hacer el post -/getTotalProductos, para traer los productos sin importar su existencia(para el cliente)

--- routes/productos.js
+++ routes/productos.js
@@ -8,12 +8,14 @@ const db =admin.database()
 
 router.post('/new', (req, res,next) => {
   console.log(req)
-  const { precio, nombre, descripcion,categoria,url } = req.body
+  const { precio, nombre, descripcion, categoria,url } = req.body
     var precioInt = parseInt(precio)
+    var existencia = true
   const newProduct = {
      precio:precioInt,
      nombre,
      descripcion,
+     existencia,
      categoria,
      url,
      id: Math.floor(Math.random() * (1 - 100000000)) + 1,
@@ -28,14 +30,33 @@ router.get('/getProductos/:categoria', async (req, res) => {
      var productos = await getProductosByCategoria(categoria)
     res.send(productos)
 })
+
 router.get('/',async (req, res) => {
   const productos = await getProductos()
 
   res.send(productos)
 })
 
+router.get('/getTotalProductos',async (req, res) => {
+  const productos = await getTotalProductos()
+
+  res.send(productos)
+})
+
+router.put('/updateExistencia', async(req,res) => {
+  const {key, existencia} = req.body
+  var producto = getProductoByKey(key)
+  producto.existencia = existencia
+
+  db.ref("productos/"+key).update(producto)
+  res.send('Actualizado')
+})
+
+
+/*ESTE GET CREO QUE PODRIA DESAPARECER
+
 router.get('/get', (req, res) => {
-     db.ref("productos").equalTo()
+     db.ref("productos").orderByChild("existencia").equalTo("true")
     .once('value', (snapshot) => {
         var productos = snapshot.val()
         console.log(productos)
@@ -43,6 +64,8 @@ router.get('/get', (req, res) => {
     })
     res.send('Creado')
   })
+  
+  */
 
 
   
@@ -59,7 +82,7 @@ router.post('/getByIds',async (req,res) =>{
 
 router.delete('/deleteByKey', async(req,res)=>{
   const {key} = req.body
-  db.ref("productos").child(key).remove()
+  await db.ref("productos").child(key).remove()
   res.send('Borrado')
 })
 
@@ -68,6 +91,7 @@ async function getProductos () {
     key: null,
     nombre: null,
     precio: null,
+    existencia: null,
     descripcion: null,
     id: null
   }
@@ -82,6 +106,7 @@ async function getProductos () {
           nombre: productos[i].nombre,
           precio: productos[i].precio,
           descripcion: productos[i].descripcion,
+          existencia: productos[i].existencia,
           id: productos[i].id,
           url:productos[i].url,
           categoria:productos[i].categoria
@@ -94,11 +119,72 @@ async function getProductos () {
 }
 
 
+async function getTotalProductos () {
+  var productoTemplateRespuesta = {
+    key: null,
+    nombre: null,
+    precio: null,
+    existencia: null,
+    descripcion: null,
+    id: null
+  }
+
+  await db.ref("productos").orderByChild("existencia").equalTo("true")
+  .once('value', (snapshot) => {
+    var productos = snapshot.val()
+    resultado = []
+      for (var i in productos){
+        productoTemplateRespuesta = {
+          key: i,
+          nombre: productos[i].nombre,
+          precio: productos[i].precio,
+          descripcion: productos[i].descripcion,
+          existencia: productos[i].existencia,
+          id: productos[i].id,
+          url:productos[i].url,
+          categoria:productos[i].categoria
+        }
+        resultado.push(productoTemplateRespuesta)
+      }
+        
+  })
+  return resultado
+}
+
+
+async function getProductoByKey (key) {
+  var productoTemplateRespuesta = {
+    nombre: null,
+    precio: null,
+    descripcion: null,
+    existencia: null,
+    id: null
+  }
+  await db.ref("productos/"+key)
+    .once('value', (snapshot) => {
+      var productos = snapshot.val()
+      resultado = []
+        for (var i in productos){
+          productoTemplateRespuesta = {
+            nombre: productos[i].nombre,
+            precio: productos[i].precio,
+            descripcion: productos[i].descripcion,
+            existencia: productos[i].existencia,
+            id: productos[i].id,
+            url:productos[i].url
+          }
+        }
+          
+    })
+    return productoTemplateRespuesta;
+}
+
 async function getProductoById (id) {
   var productoTemplateRespuesta = {
     nombre: null,
     precio: null,
     descripcion: null,
+    existencia: null,
     id: null
   }
   await db.ref("productos").orderByChild("id").equalTo(id)
@@ -110,6 +196,7 @@ async function getProductoById (id) {
             nombre: productos[i].nombre,
             precio: productos[i].precio,
             descripcion: productos[i].descripcion,
+            existencia: productos[i].existencia,
             id: productos[i].id,
             url:productos[i].url
           }
@@ -124,6 +211,7 @@ async function getProductosByCategoria (categoria) {
     precio: null,
     descripcion: null,
     categoria: null,
+    existencia: null,
     id: null,
     url: null
   }
@@ -138,6 +226,7 @@ async function getProductosByCategoria (categoria) {
             nombre: productos[i].nombre,
             precio: productos[i].precio,
             descripcion: productos[i].descripcion,
+            existencia: productos[i].existencia,
             id: productos[i].id,
             url:productos[i].url,
             categoria:productos[i].categoria
